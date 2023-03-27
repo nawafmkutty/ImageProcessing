c648b1f7f59f328b106a306fc86709c5a1094964
mcsonline <nawafmkutty@gmail.com>
2023-03-27 16:53:59 +0300

    fully done project

--- image.c
+++ image.c
@@ -45,12 +45,95 @@ void image_apply_bw(Image* img) {
 
     for(int h = 0 ; h< img->height ; h++) {
         for (int w = 0; w < img->width; w++) {
-                img->pArr[h][w].red = img->pArr[h][w].red * rGrayScale;
-                img->pArr[h][w].blue = img->pArr[h][w].blue * bGrayScale;
-                img->pArr[h][w].green = img->pArr[h][w].green * gGrayScale;
+
+            int x  = (img->pArr[h][w].red + img->pArr[h][w].blue + img->pArr[h][w].green) / 3 ;
+
+            img->pArr[h][w].red = x;
+            img->pArr[h][w].blue = x;
+            img->pArr[h][w].green = x;
+
+//                img->pArr[h][w].red = img->pArr[h][w].red * rGrayScale;
+//                img->pArr[h][w].blue = img->pArr[h][w].blue * bGrayScale;
+//                img->pArr[h][w].green = img->pArr[h][w].green * gGrayScale;
         }
 
     }
 
 
+}
+
+int image_get_height(Image* img) {
+    return img->height;
+}
+
+int image_get_width(Image* img){
+    return img->width;
+}
+
+struct Pixel** image_get_pixels(Image* img) {
+    return img->pArr;
+}
+
+void image_destroy(Image** img) {
+
+    free(img);
+
+//    int i = 0;
+//    while(img[i] != NULL)
+//    {
+//    for(int h = 0 ; h< img[i]->height  ; h++) {
+//        for (int w = 0; w < img[i]->width ; w++) {
+//            free(img[h][w].pArr);
+//        }
+//        free(img[h]);
+//        }
+//}
+
+
+}
+
+
+
+void image_apply_resize(Image* img, float factor){
+
+    int newImageWidth = img->width * factor;
+    int newImageHeight = img->height * factor;
+
+//    img->height = img->height * factor;
+//    img->width = img->width * factor;
+
+    struct Pixel** pixelArray = (struct Pixel**) malloc(img->height * factor * sizeof(struct Pixel*));
+
+    for(int i = 0 ; i < img->height * factor; i++ ) {
+        pixelArray[i] = (struct Pixel*) malloc(img->width * factor * sizeof(struct Pixel));
+    }
+
+    struct Pixel** IgImagepixelArray = img->pArr;
+    for(int h = 0 ; h< img->height * factor ; h++) {
+        for (int w = 0; w < img->width * factor; w++) {
+
+            int og_h = h/factor;
+            int og_w = w/factor;
+
+        pixelArray[h][w].red = IgImagepixelArray[og_h][og_w].red;
+        pixelArray[h][w].green = IgImagepixelArray[og_h][og_w].green;
+        pixelArray[h][w].blue = IgImagepixelArray[og_h][og_w].blue;
+
+        }}
+
+    img->height = newImageHeight;
+    img->width = newImageWidth;
+    img->pArr = pixelArray;
+    free(IgImagepixelArray);
+
+}
+
+
+Image* image_create(struct Pixel** pArr, int width, int height) {
+    struct Image *newImage = malloc(sizeof(struct Image));
+    newImage->pArr = pArr;
+    newImage->width = width;
+    newImage->height=height;
+
+    return newImage;
 }
--- main.c
+++ main.c
@@ -4,59 +4,151 @@
 void ProcessOption(int option);
 void userMenu();
 
-int main() {
+struct ColorFilter {
+    int red;
+    int blue;
+    int green;
+};
 
-    FILE *file_input = fopen("ttt.bmp","rb");
+struct userArguments {
+    char inputImageName[20];
+    int scale;
+    float scaleFactor;
+    int grayScale;
+    int colorFilter;
+    struct ColorFilter filterValues;
+    int output;
+    char outputFileName[20];
+
+};
+
+
+int main(int argc , char *argv[]) {
+
+struct userArguments ua;
+    strcpy(ua.inputImageName,argv[1]);
+    ua.filterValues.red = 0;
+    ua.filterValues.green = 0;
+    ua.filterValues.blue = 0;
+    ua.grayScale = 0;
+    ua.colorFilter = 0;
+    ua.scale = 0;
+
+
+    for(int i = 2 ; i < argc ; i++ ) {
+
+        if (strcmp(argv[i],"-w") == 0) {
+            ua.grayScale = 1;
+        }
+        else if (strcmp(argv[i],"-s") == 0) {
+            ua.scale = 1;
+            i++;
+            ua.scaleFactor = atof(argv[i]);
+        } else if (strcmp(argv[i],"-r") == 0) {
+            ua.colorFilter = 1;
+            i++;
+            ua.filterValues.red = atoi(argv[i]);
+        }else if (strcmp(argv[i],"-g") == 0) {
+            ua.colorFilter = 1;
+            i++;
+            ua.filterValues.green = atoi(argv[i]);
+        } else if (strcmp(argv[i],"-b") == 0){
+            ua.colorFilter = 1;
+            i++;
+            ua.filterValues.blue = atoi(argv[i]);
+        } else if (strcmp(argv[i],"-o") == 0){
+            ua.output = 1;
+            i++;
+            strcpy(ua.outputFileName,argv[i]);
+
+        }
+    }
+
+
+    printf("The Parameters are \n");
+    printf("Input File: %s \n",ua.inputImageName);
+    printf("Gray scale Flag is :%d\n",ua.grayScale);
+    printf("scale Flag is :%d\n",ua.scale);
+    printf("scale Factor is :%f\n",ua.scaleFactor);
+    printf("Color Filter Flag is :%d\n",ua.colorFilter);
+    printf("Color Filter is R:%d , G:%d ,B:%d \n",ua.filterValues.red,ua.filterValues.green,ua.filterValues.blue);
+    printf("Output flag is :%d\n",ua.output);
+    printf("Output File: %s \n",ua.outputFileName);
+
+
+    if(ua.inputImageName != NULL) {
+
+        FILE *file_input = fopen(ua.inputImageName, "rb");
+        if(file_input == NULL) {
+            printf("Input file is not available \n");
+            return EXIT_FAILURE;
+        }
+
+//    FILE *file_input = fopen("ttt.bmp", "rb");
     struct BMP_Header *header = malloc(sizeof(struct BMP_Header));
-
-    readBMPHeader(file_input,header);
+    readBMPHeader(file_input, header);
     struct DIB_Header *dibHeader = malloc(sizeof(struct DIB_Header));
-    readDIBHeader(file_input,dibHeader);
+    readDIBHeader(file_input, dibHeader);
 
-    struct Image* workingImage = malloc(sizeof(struct Image));
+    struct Image *workingImage = malloc(sizeof(struct Image));
 
     workingImage->height = dibHeader->ImageHeight;
     workingImage->width = dibHeader->ImageWidth;
 
-    struct Pixel** pixelArray = (struct Pixel**) malloc(workingImage->height * sizeof(struct Pixel*));
+    struct Pixel **pixelArray = (struct Pixel **) malloc(workingImage->height * sizeof(struct Pixel *));
 
-    for(int i = 0 ; i < workingImage->height; i++ ) {
-        pixelArray[i] = (struct Pixel*) malloc(workingImage->width * sizeof(struct Pixel));
+    for (int i = 0; i < workingImage->height; i++) {
+        pixelArray[i] = (struct Pixel *) malloc(workingImage->width * sizeof(struct Pixel));
     }
 
-    readPixelsBMP(file_input,pixelArray,workingImage->width,workingImage->height);
-    workingImage->pArr =pixelArray;
+    readPixelsBMP(file_input, pixelArray, workingImage->width, workingImage->height);
+    workingImage->pArr = pixelArray;
     printf("The Values are: \n\n");
-    for(int h = 0 ; h< workingImage->height ; h++){
-        for(int w =0 ; w < workingImage->width; w++) {
-
-            printf("[ R:%u,G:%u,B%u ]",pixelArray[h][w].red,pixelArray[h][w].green,pixelArray[h][w].blue);
-
-        }
-        printf("\n");
-    }
-
-
+//    for (int h = 0; h < workingImage->height; h++) {
+//        for (int w = 0; w < workingImage->width; w++) {
+//
+//            printf("[ R:%u,G:%u,B:%u ]", pixelArray[h][w].red, pixelArray[h][w].green, pixelArray[h][w].blue);
+//
+//        }
+//        printf("\n");
+//    }
     fclose(file_input);
 
+        if(ua.grayScale == 1) {
+            image_apply_bw(workingImage);
+        }
 
-    //image_apply_colorshift(workingImage,56,45,78);
-    image_apply_bw(workingImage);
+        if (ua.colorFilter == 1) {
+            image_apply_colorshift(workingImage,ua.filterValues.red,ua.filterValues.green,ua.filterValues.blue);
+        }
 
-    FILE *file_out = fopen("ttt_Nawaf_copy_bw.bmp","wb");
+        if (ua.scale == 1) {
+            image_apply_resize(workingImage,ua.scaleFactor);
+            dibHeader->ImageWidth =workingImage->width;
+            dibHeader->ImageHeight =workingImage->height;
+        }
+        FILE *file_out;
+        if (ua.output == 1) {
+            file_out = fopen(ua.outputFileName, "wb");
+        } else {
+            file_out = fopen("inputImage_copy.bmp", "wb");
+        }
 
-    writeBMPHeader(file_out,header);
-    writeDIBHeader(file_out,dibHeader);
-    writePixelsBMP(file_out,workingImage->pArr,workingImage->width,workingImage->height);
+
+    writeBMPHeader(file_out, header);
+    writeDIBHeader(file_out, dibHeader);
+    writePixelsBMP(file_out, workingImage->pArr, workingImage->width, workingImage->height);
     fclose(file_out);
-
-
-
 
 
     free(header);
     free(dibHeader);
-    free(pixelArray);
-    return 0;
+
+    image_destroy(workingImage);
+    } else {
+        printf("Error in File Name \n");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
